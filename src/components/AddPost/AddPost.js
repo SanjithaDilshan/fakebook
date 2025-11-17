014772d30438eabec1e6e6a6a63b6f088e69a389
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-11-17 10:50:40 +0530

    Add Post Component CSS part

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./addPost.css";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
 
 export default function AddPost() {
   return (
@@ -7,8 +11,37 @@ export default function AddPost() {
       <div className="addPostContainer">
         <div className="addPostTop">
           <img src="/images/1.jpeg" alt="" className="addPostPic" />
+          <input
+            placeholder="What's in your mind...? "
+            type="text"
+            className="addPostInput"
+          />
         </div>
-        <div className="addPostBottom"></div>
+        <hr className="postHr" />
+        <div className="addPostBottom">
+          <div className="addPostOptions">
+            <div className="addPostOption">
+              <AddPhotoAlternateIcon htmlColor="orange" className="addPhoto" />
+              <span className="addPostOptionText">Add Photo/Video</span>
+            </div>
+
+            <div className="addPostOption">
+              <AddLocationAltIcon htmlColor="red" className="addPhoto" />
+              <span className="addPostOptionText">Add Location</span>
+            </div>
+
+            <div className="addPostOption">
+              <LocalOfferIcon htmlColor="blue" className="addPhoto" />
+              <span className="addPostOptionText">Add Tag</span>
+            </div>
+
+            <div className="addPostOption">
+              <LiveTvIcon htmlColor="tomato" className="addPhoto" />
+              <span className="addPostOptionText">Go Live</span>
+            </div>
+          </div>
+          <button className="PostButton">Post</button>
+        </div>
       </div>
     </div>
   );
