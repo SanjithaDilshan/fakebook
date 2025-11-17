578f476ecac3be8cc32fbb2742092878aca026c2
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-11-17 19:13:55 +0530

    Birthdays suggestion,advertisement,all parts designed

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -2,5 +2,126 @@ import React from "react";
 import "./RightPane.css";
 
 export default function RightPane() {
-  return <div className="rightPaneBox">RightPane</div>;
+  return (
+    <div className="rightPaneBox">
+      <div className="rightPaneContainer">
+        <div className="adContainer">
+          <span className="sponsered">Sponsored</span>
+          <img src="/images/adImage.jpg" alt="" className="adImage" />
+          <sapn className="adText">
+            Order Your Food with Code with Sanju Food Delivery Service. We can
+            deliver your food into your door-step within seconds. Order Now...!
+          </sapn>
+        </div>
+        <div className="birthdayContainer">
+          <img
+            src="/images/birthdayImage.jpg"
+            alt=""
+            className="birthdayImage"
+          />
+          <span className="birthdayText">
+            <b>Sanjitha Dilshan</b> and <b>2 others</b> having birthday
+            today...!
+          </span>
+        </div>
+        <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img
+                src="/images/friend.avif"
+                alt=""
+                className="onlineFriendImg"
+              />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Luci Amelia Charlotte</span>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 }
